25ce259609cd55e05e7c90d4057d0b89d52c6c7b
Sua Jun <mjun3712@gmail.com>
2025-05-19 19:46:38 +0900

    Refactor: 게시글 생성 및 전체 조회 로직 리펙토링

--- src/main/java/likelion13th/blog/Controller/ArticleController.java
+++ src/main/java/likelion13th/blog/Controller/ArticleController.java
@@ -2,6 +2,7 @@ package likelion13th.blog.Controller;
 
 
 import likelion13th.blog.domain.Article;
+import likelion13th.blog.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,30 +15,26 @@ import static javax.security.auth.callback.ConfirmationCallback.OK;
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
-    private final List<Article> articleDB = new ArrayList<>();
-    private Long nextId = 1L;
+    private final ArticleService articleService;
 
-@PostMapping
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+@PostMapping()
     public ResponseEntity<Article> createArticle(@RequestBody Article article){
-        Article newArticle = new Article(
-                nextId++,
-                article.getContent(),
-                article.getTitle(),
-                article.getAuthor(),
-                article.getPassword()
-        );
 
-        articleDB.add(newArticle);
+        Article newArticle = articleService.addArticle(article);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(newArticle);
     }
-@GetMapping
+@GetMapping()
     public ResponseEntity<List<Article>> getArticle(){
-
+        List<Article> articles = articleService.findAll();
     return ResponseEntity
             .status(HttpStatus.OK)
-            .body(articleDB);
+            .body(articles);
     }
 }
