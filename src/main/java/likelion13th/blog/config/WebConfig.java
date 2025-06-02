841cf8ad4931556d938781277c84bbb12c856d0c
Sua Jun <mjun3712@gmail.com>
2025-06-02 19:17:50 +0900

    Feat: CORS 설정 추가2

--- src/main/java/likelion13th/blog/config/WebConfig.java
+++ src/main/java/likelion13th/blog/config/WebConfig.java
@@ -1,6 +1,6 @@
 package likelion13th.blog.config;
 
-
+//.
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
